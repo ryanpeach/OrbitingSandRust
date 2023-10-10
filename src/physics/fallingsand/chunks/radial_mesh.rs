92050a279f8bdccf1b7215e60121e5a1504fc2e8
Ryan <ryanpeach@icloud.com>
2023-10-09 22:12:29 -0400

    The FR is back to 60fps

--- src/physics/fallingsand/chunks/radial_mesh.rs
+++ src/physics/fallingsand/chunks/radial_mesh.rs
@@ -28,7 +28,8 @@ impl RadialMeshBuilder {
             num_layers: 1,
             first_num_radial_lines: 6,
             second_num_concentric_circles: 2,
-            max_cells: 2usize.pow(8).pow(2),
+            max_cells: usize::MAX,
+            // max_cells: 2usize.pow(16).pow(2),
         }
     }
 
