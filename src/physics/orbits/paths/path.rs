f85d80528cdaff99f7f384f2639de383d556b8c4
Ryan Peach <rgpeach10@gmail.com>
2024-01-13 22:26:42 -0500

    More documentation

--- src/physics/orbits/paths/path.rs
+++ src/physics/orbits/paths/path.rs
@@ -1,9 +1,12 @@
+//! Paths are the main component of the orbiting system.
+//! They allow precomputation of orbits, allowing them to be stored in memory
+
 use bevy::ecs::component::Component;
 use glam::Vec2;
 
 use crate::physics::util::clock::Clock;
 
-/// A path is a series of points that an object will follow. \
+/// A path is a series of points that an object will follow.
 /// The object will move from one point to the next in a straight line,
 /// and will move at a constant speed.
 #[derive(Component)]
