a81bcd2d104932dd107b11d3e66cbcc779fd215e
Ryan Peach <ryanpeach@me.com>
2024-01-31 22:46:16 -0500

    Deleting benches

--- benches/bench_main.rs
+++
@@ -1,8 +0,0 @@
-use criterion::criterion_main;
-
-mod physics;
-
-criterion_main! {
-    physics::fallingsand::mesh::coordinate_directory::benches,
-    physics::fallingsand::data::element_directory::benches,
-}
