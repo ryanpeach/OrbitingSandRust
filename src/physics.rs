006e3635df5cb13cf3ebcd06dc633ed3776d98ef
Ryan Peach <ryanpeach@me.com>
2024-02-02 00:11:11 -0500

    Remove game engine agnostic warning, just commit to bevy

--- src/physics.rs
+++ src/physics.rs
@@ -1,10 +1,4 @@
 //! This module contains all the physics related code.
-//!
-//! When contributing to this module, please keep the following things in mind:
-//! * I try to maintain this module as "game engine agnostic" as possible.
-//!   That way it is usable in other projects and if we ever decide to switch game engines.
-//!   Please use internal types as much as possible.
-//! * Physics should be highly unit tested.
 
 use bevy::app::{PluginGroup, PluginGroupBuilder};
 
