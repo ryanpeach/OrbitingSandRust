fe2742e8c65ad4a967d66f52ded5746eb684611a
Ryan Peach <ryanpeach@me.com>
2024-02-01 22:05:46 -0500

    Created the basics for the gravity field renderer

--- assets/shaders/gravity_vis.wgsl
+++ assets/shaders/gravity_vis.wgsl
@@ -3,20 +3,14 @@
 // Bodies which emit gravity effect each other
 // Bodies which do not emit gravity are only effected by bodies which do emit gravity
 
-struct GravityField {
-    g: f32;
-};
+#import orbiting_sand_rust::math::fast_length_2d;
 
-@group(1) @binding(0) var<uniform> material: GravityField;
-@group(1) @binding(1) var<storage, read> positions: array<vec2<f32>>;
-@group(2) @binding(2) var<storage, read> masses: array<f32>;
+@group(1) @binding(0) var<storage, read> positions: array<vec2<f32>>;
+@group(2) @binding(1) var<storage, read> masses: array<f32>;
 
-fn computeGravitationalForce(pos1: vec2<f32>, pos2: vec2<f32>, mass2: f32) -> vec2<f32> {
+fn computeGravitationalVector(pos1: vec2<f32>, pos2: vec2<f32>, mass2: f32) -> vec2<f32> {
     let r = pos2.xy - pos1.xy;
-    let distanceSquared = dot(r, r);
-    let forceMagnitude = material.g * mass2 / distanceSquared;
-    let forceDirection = normalize(r);
-    return forceDirection * forceMagnitude;
+    return (r * mass2) / (fast_length_2d(r) * dot(r, r));
 }
 
 struct VertexOutput {
@@ -30,7 +24,7 @@ fn fs_main(@builtin(position) fragCoord: vec4<f32>) -> @location(0) vec4<f32> {
     let pos = fragCoord.xy; // Assuming fragCoord.xy gives us the position in some space where we calculate forces
 
     for (var i = 0u; i < grav_bodies.length(); i++) {
-        let force = computeGravitationalForce(vec3<f32>(pos, 0.0), vec3<f32>(grav_bodies[i].position, 0.0), grav_bodies[i].mass);
+        let force = computeGravitationalVector(vec3<f32>(pos, 0.0), vec3<f32>(grav_bodies[i].position, 0.0), grav_bodies[i].mass);
         totalForce += force;
     }
 
@@ -43,7 +37,8 @@ fn fs_main(@builtin(position) fragCoord: vec4<f32>) -> @location(0) vec4<f32> {
     let colorIntensity = level / levels;
 
     // Color mapping based on contour level
-    let color = vec4<f32>(colorIntensity, colorIntensity, 1.0 - colorIntensity, 1.0);
+    // Alpha should be the intensity
+    let color = vec4<f32>(1.0, 0.0, 0.0, colorIntensity);
 
     return color;
 }
--- src/physics/orbits/gravity_vis.rs
+++ src/physics/orbits/gravity_vis.rs
@@ -1,14 +1,16 @@
+//! This module creates a visualizer for gravity fields.
+#![deny(missing_docs)]
+#![deny(clippy::missing_docs_in_private_items)]
+
 use bevy::{
     app::{App, Plugin, Startup, Update},
-    asset::{Asset, Assets},
-    core_pipeline::core_2d::Camera2d,
+    asset::{Asset, Assets, Handle},
     ecs::{
         component::Component,
-        entity::Entity,
+        event::EventReader,
         query::With,
         system::{Commands, Query, ResMut},
     },
-    hierarchy::BuildChildren,
     math::Vec2,
     reflect::TypePath,
     render::{
@@ -17,37 +19,32 @@ use bevy::{
     },
     sprite::{Material2d, MaterialMesh2dBundle},
     transform::components::Transform,
-    window::Window,
+    window::{Window, WindowResized},
 };
 
-use super::{
-    components::{GravitationalField, Mass},
-    nbody::G,
-};
+use super::components::{GravitationalField, Mass};
 
-#[derive(Component, Debug, Clone, Copy)]
-pub struct GravityField;
-
-#[derive(AsBindGroup, Debug, Clone, Asset, TypePath)]
-pub struct GravityFieldBindGroup {
-    #[uniform(0)]
-    g: f32,
-
-    #[storage(1)]
-    positions: Vec<Vec2>,
-
-    #[storage(2)]
-    masses: Vec<f32>,
+/// Identifies that this entity is an image to be overlayed on the screen.
+#[derive(Component, Debug, Clone)]
+pub struct ImageOverlay {
+    /// The priority of the image. High numbers are drawn on top of low numbers.
+    pub priority: i32,
 }
 
-impl Default for GravityFieldBindGroup {
-    fn default() -> Self {
-        GravityFieldBindGroup {
-            g: G,
-            positions: Vec::new(),
-            masses: Vec::new(),
-        }
-    }
+/// Identifies that this entity is a gravity field.
+#[derive(Component, Debug, Clone)]
+pub struct GravityField;
+
+/// Binds the positions and masses of all gravitational bodies to the shader.
+#[derive(AsBindGroup, Default, Debug, Clone, Asset, TypePath)]
+pub struct GravityFieldBindGroup {
+    /// The positions of each gravitational body.
+    #[storage(1)]
+    pub positions: Vec<Vec2>,
+
+    /// The mass of each gravitational body.
+    #[storage(2)]
+    pub masses: Vec<f32>,
 }
 
 // All functions on `Material2d` have default impls. You only need to implement the
@@ -60,52 +57,41 @@ impl Material2d for GravityFieldBindGroup {
     fn vertex_shader() -> ShaderRef {
         ShaderRef::Default
     }
-
-    fn specialize(
-        _descriptor: &mut bevy::render::render_resource::RenderPipelineDescriptor,
-        _layout: &bevy::render::mesh::MeshVertexBufferLayout,
-        _key: bevy::sprite::Material2dKey<Self>,
-    ) -> Result<(), bevy::render::render_resource::SpecializedMeshPipelineError> {
-        Ok(())
-    }
 }
 
+/// Creates a gravity field overlay.
 pub struct GravityFieldPlugin;
 
 impl Plugin for GravityFieldPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, GravityFieldPlugin::setup);
         app.add_systems(Update, GravityFieldPlugin::update_gravity_well);
+        app.add_systems(Update, GravityFieldPlugin::window_resized);
     }
 }
 
 impl GravityFieldPlugin {
-    // Spawn an entity using `CustomMaterial`.
+    /// Create the gravity field entity.
     fn setup(
         mut commands: Commands,
         mut materials: ResMut<Assets<GravityFieldBindGroup>>,
         mut meshes: ResMut<Assets<Mesh>>,
-        camera: Query<Entity, With<Camera2d>>,
         windows: Query<&Window>,
     ) {
         let window = windows.single();
         let window_size = Vec2::new(window.width(), window.height());
-        let entity = commands
-            .spawn((
-                MaterialMesh2dBundle {
-                    material: materials.add(GravityFieldBindGroup::default()),
-                    mesh: meshes.add(shape::Quad::new(window_size).into()).into(),
-                    ..Default::default()
-                },
-                GravityField,
-            ))
-            .id();
-        // Add the entity as a child of the camera.
-        for camera in camera.iter() {
-            commands.entity(camera).push_children(&[entity]);
-        }
+        commands.spawn((
+            MaterialMesh2dBundle {
+                material: materials.add(GravityFieldBindGroup::default()),
+                mesh: meshes.add(shape::Quad::new(window_size).into()).into(),
+                ..Default::default()
+            },
+            GravityField,
+            ImageOverlay { priority: 0 },
+        ));
     }
 
+    /// Update the gravity field overlay with the positions and masses of all gravitational bodies.
     fn update_gravity_well(
         mut materials: ResMut<Assets<GravityFieldBindGroup>>,
         gravity_bodies: Query<(&Transform, &Mass), With<GravitationalField>>,
@@ -119,5 +105,20 @@ impl GravityFieldPlugin {
         }
         gravity_field.positions = positions;
         gravity_field.masses = masses;
+    }
+
+    /// Update the size of the gravity field overlay when the window is resized.
+    fn window_resized(
+        mut meshes: ResMut<Assets<Mesh>>,
+        windows: Query<&Window>,
+        mut gravity_field: Query<&Handle<Mesh>, With<GravitationalField>>,
+        _window_resized_event: EventReader<WindowResized>,
+    ) {
+        let window = windows.single();
+        let window_size = Vec2::new(window.width(), window.height());
+        for mesh in gravity_field.iter_mut() {
+            let mesh = meshes.get_mut(mesh).unwrap();
+            *mesh = shape::Quad::new(window_size).into();
+        }
     }
 }
