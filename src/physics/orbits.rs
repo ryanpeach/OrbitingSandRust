15ac37edb5071e8ffee398bb1ff43d906f9350b7
Ryan Peach <ryanpeach@me.com>
2024-02-09 23:43:39 -0500

    force docs in orbits module

--- src/physics/orbits.rs
+++ src/physics/orbits.rs
@@ -1,4 +1,6 @@
 //! Orbits and orbital mechanics physics engine.
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
 
 /// Most of the units of measure as well as bevy components for orbits and orbital mechanics.
 pub mod components;
