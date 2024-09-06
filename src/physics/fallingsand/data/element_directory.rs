6495d314e5fc6c73ec3abc47edb90c5c84db0def
Ryan Peach <git.strenuous149@passmail.net>
2024-09-06 17:09:01 -0400

    fix: cargo fmt

--- src/physics/fallingsand/data/element_directory.rs
+++ src/physics/fallingsand/data/element_directory.rs
@@ -210,8 +210,12 @@ fn pregen_process_targets(coords: &CoordinateDir) -> ProcessTargets {
     let mut has_single_bottom_neighbor: [Sequential<HashSet<ChunkIjkVector>>; 9] =
         Default::default();
     let mut has_multi_bottom_neighbor: [Parallel<HashSet<ChunkIjkVector>>; 9] = Default::default();
-    for (i, (standard, single, multi)) in multizip((&mut standard_convolution, &mut has_single_bottom_neighbor, &mut has_multi_bottom_neighbor))
-        .enumerate()
+    for (i, (standard, single, multi)) in multizip((
+        &mut standard_convolution,
+        &mut has_single_bottom_neighbor,
+        &mut has_multi_bottom_neighbor,
+    ))
+    .enumerate()
     {
         *standard = calculate_ith_standard_convolution_targets(coords, i);
         *single = calculate_ith_has_single_bottom_neighbor_targets(coords, i);
